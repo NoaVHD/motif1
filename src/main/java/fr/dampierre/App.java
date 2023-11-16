8264637583cd6db912d66b18405e75be7c420a21
NoaVHD <noavanhoorde@gmail.com>
2023-11-16 09:06:47 +0100

    first commit

---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,25 @@
+package fr.dampierre;
+
+public class App {
+    
+    public static void main(String[] args) {
+        int nbColone =40;
+        for(int i = 1; i<=nbColone;i++){
+            System.out.print("-");
+        }
+        System.out.println();
+        for(int i=1; i<=10;i++){
+            System.out.print("_-^-");
+        }
+        System.out.println();
+        for(int i=1; i<=2;i++){
+            System.out.print("11223344556677889900");
+        }
+        System.out.println();
+        for(int i = 1; i<=nbColone;i++){
+            System.out.print("-");
+        }
+        
+    }
+    
+}
